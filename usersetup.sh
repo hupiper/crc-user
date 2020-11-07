07290efed24c206ff3b4bfaa6b2b2af66918a992
Mark Dunnett <mdunnett@charter.net>
2020-11-07 11:27:29 -0600

    cleaned up files

--- usersetup.sh
+++ usersetup.sh
@@ -5,6 +5,6 @@ htpasswd -b ${TMPHTPASS} admin 'admin'
 htpasswd -b ${TMPHTPASS} developer 'developer'
 htpasswd -b ${TMPHTPASS} user 'user'
 
-oc -n openshift-config delete secret workshop-user-secret
+oc -n openshift-config delete secret user-secret
 
-oc -n openshift-config create secret generic workshop-user-secret --from-file=htpasswd=${TMPHTPASS}
+oc -n openshift-config create secret generic user-secret --from-file=htpasswd=${TMPHTPASS}
